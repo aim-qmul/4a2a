74f4d13f57da6ad15ec70114b9f2debc8fa02047
Scusk Rimsi <chin-yun.yu@qmul.ac.uk>
2024-12-02 17:16:13 +0800

    feat: partial_sum for sequential logic (#2)
    * feat: partial_sum for sequential logic, remove gPrev
    
    * refactor
    
    * Update .gitignore
    
    * update juce project file
    
    * apply formater
    
    * update ignore
    
    * use mac formater

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -1,7 +1,7 @@
 /*
   ==============================================================================
 
-    This file contains the basic framework code for a JUCE plugin editor.
+	This file contains the basic framework code for a JUCE plugin editor.
 
   ==============================================================================
 */
@@ -11,137 +11,137 @@
 
 //==============================================================================
 _4A2AAudioProcessorEditor::_4A2AAudioProcessorEditor(
-    _4A2AAudioProcessor &p, juce::AudioProcessorValueTreeState &vts)
-    : AudioProcessorEditor(&p), audioProcessor(p), valueTreeState(vts)
+	_4A2AAudioProcessor &p, juce::AudioProcessorValueTreeState &vts)
+	: AudioProcessorEditor(&p), audioProcessor(p), valueTreeState(vts)
 {
-    // Make sure that before the constructor has finished, you've set the
-    // editor's size to whatever you need it to be.
-    setSize(400, 300);
+	// Make sure that before the constructor has finished, you've set the
+	// editor's size to whatever you need it to be.
+	setSize(400, 300);
 
-    peakReduction.setRange(40, 100);
-    peakReduction.setValue(40);
+	peakReduction.setRange(40, 100);
+	peakReduction.setValue(40);
 
-    peakReduction.setTextBoxStyle(juce::Slider::TextBoxLeft, false, 120,
-                                  peakReduction.getTextBoxHeight());
-    threshold.setTextBoxStyle(juce::Slider::TextBoxLeft, false, 120,
-                              threshold.getTextBoxHeight());
-    ratio.setTextBoxStyle(juce::Slider::TextBoxLeft, false, 120,
-                          ratio.getTextBoxHeight());
-    attackMs.setTextBoxStyle(juce::Slider::TextBoxLeft, false, 120,
-                             attackMs.getTextBoxHeight());
-    releaseMs.setTextBoxStyle(juce::Slider::TextBoxLeft, false, 120,
-                              releaseMs.getTextBoxHeight());
-    makeUp.setTextBoxStyle(juce::Slider::TextBoxLeft, false, 120,
-                           makeUp.getTextBoxHeight());
+	peakReduction.setTextBoxStyle(juce::Slider::TextBoxLeft, false, 120,
+								  peakReduction.getTextBoxHeight());
+	threshold.setTextBoxStyle(juce::Slider::TextBoxLeft, false, 120,
+							  threshold.getTextBoxHeight());
+	ratio.setTextBoxStyle(juce::Slider::TextBoxLeft, false, 120,
+						  ratio.getTextBoxHeight());
+	attackMs.setTextBoxStyle(juce::Slider::TextBoxLeft, false, 120,
+							 attackMs.getTextBoxHeight());
+	releaseMs.setTextBoxStyle(juce::Slider::TextBoxLeft, false, 120,
+							  releaseMs.getTextBoxHeight());
+	makeUp.setTextBoxStyle(juce::Slider::TextBoxLeft, false, 120,
+						   makeUp.getTextBoxHeight());
 
-    peakReductionLabel.setText("Peak Reduction", juce::dontSendNotification);
-    peakReductionLabel.attachToComponent(&peakReduction, true);
+	peakReductionLabel.setText("Peak Reduction", juce::dontSendNotification);
+	peakReductionLabel.attachToComponent(&peakReduction, true);
 
-    thresholdLabel.setText("Threshold", juce::dontSendNotification);
-    thresholdLabel.attachToComponent(&threshold, true);
-    threshold.setTextValueSuffix(" dB");
+	thresholdLabel.setText("Threshold", juce::dontSendNotification);
+	thresholdLabel.attachToComponent(&threshold, true);
+	threshold.setTextValueSuffix(" dB");
 
-    ratioLabel.setText("Ratio", juce::dontSendNotification);
-    ratioLabel.attachToComponent(&ratio, true);
+	ratioLabel.setText("Ratio", juce::dontSendNotification);
+	ratioLabel.attachToComponent(&ratio, true);
 
-    attackMsLabel.setText("Attack", juce::dontSendNotification);
-    attackMsLabel.attachToComponent(&attackMs, true);
-    attackMs.setTextValueSuffix(" ms");
+	attackMsLabel.setText("Attack", juce::dontSendNotification);
+	attackMsLabel.attachToComponent(&attackMs, true);
+	attackMs.setTextValueSuffix(" ms");
 
-    releaseMsLabel.setText("Release", juce::dontSendNotification);
-    releaseMsLabel.attachToComponent(&releaseMs, true);
-    releaseMs.setTextValueSuffix(" ms");
+	releaseMsLabel.setText("Release", juce::dontSendNotification);
+	releaseMsLabel.attachToComponent(&releaseMs, true);
+	releaseMs.setTextValueSuffix(" ms");
 
-    makeUpLabel.setText("Make-up", juce::dontSendNotification);
-    makeUpLabel.attachToComponent(&makeUp, true);
-    makeUp.setTextValueSuffix(" dB");
+	makeUpLabel.setText("Make-up", juce::dontSendNotification);
+	makeUpLabel.attachToComponent(&makeUp, true);
+	makeUp.setTextValueSuffix(" dB");
 
-    peakReduction.onValueChange = [this]
-    {
-        auto params = interp(peakReduction.getValue());
-        threshold.setValue(params[0]);
-        ratio.setValue(params[1]);
-        attackMs.setValue(params[2]);
-        releaseMs.setValue(params[3]);
-        makeUp.setValue(params[4]);
-    };
+	peakReduction.onValueChange = [this]
+	{
+		auto params = interp(peakReduction.getValue());
+		threshold.setValue(params[0]);
+		ratio.setValue(params[1]);
+		attackMs.setValue(params[2]);
+		releaseMs.setValue(params[3]);
+		makeUp.setValue(params[4]);
+	};
 
-    addAndMakeVisible(&peakReduction);
-    addAndMakeVisible(&peakReductionLabel);
-    addAndMakeVisible(&threshold);
-    addAndMakeVisible(&thresholdLabel);
-    addAndMakeVisible(&ratio);
-    addAndMakeVisible(&ratioLabel);
-    addAndMakeVisible(&attackMs);
-    addAndMakeVisible(&attackMsLabel);
-    addAndMakeVisible(&releaseMs);
-    addAndMakeVisible(&releaseMsLabel);
-    addAndMakeVisible(&makeUp);
-    addAndMakeVisible(&makeUpLabel);
+	addAndMakeVisible(&peakReduction);
+	addAndMakeVisible(&peakReductionLabel);
+	addAndMakeVisible(&threshold);
+	addAndMakeVisible(&thresholdLabel);
+	addAndMakeVisible(&ratio);
+	addAndMakeVisible(&ratioLabel);
+	addAndMakeVisible(&attackMs);
+	addAndMakeVisible(&attackMsLabel);
+	addAndMakeVisible(&releaseMs);
+	addAndMakeVisible(&releaseMsLabel);
+	addAndMakeVisible(&makeUp);
+	addAndMakeVisible(&makeUpLabel);
 
-    thresholdAttachment.reset(
-        new SliderAttachment(valueTreeState, "th", threshold));
-    ratioAttachment.reset(new SliderAttachment(valueTreeState, "ratio", ratio));
-    attackMsAttachment.reset(
-        new SliderAttachment(valueTreeState, "at", attackMs));
-    releaseMsAttachment.reset(
-        new SliderAttachment(valueTreeState, "rt", releaseMs));
-    makeUpAttachment.reset(
-        new SliderAttachment(valueTreeState, "makeUp", makeUp));
+	thresholdAttachment.reset(
+		new SliderAttachment(valueTreeState, "th", threshold));
+	ratioAttachment.reset(new SliderAttachment(valueTreeState, "ratio", ratio));
+	attackMsAttachment.reset(
+		new SliderAttachment(valueTreeState, "at", attackMs));
+	releaseMsAttachment.reset(
+		new SliderAttachment(valueTreeState, "rt", releaseMs));
+	makeUpAttachment.reset(
+		new SliderAttachment(valueTreeState, "makeUp", makeUp));
 }
 
 _4A2AAudioProcessorEditor::~_4A2AAudioProcessorEditor()
 {
-    thresholdAttachment.reset();
-    ratioAttachment.reset();
-    attackMsAttachment.reset();
-    releaseMsAttachment.reset();
-    makeUpAttachment.reset();
+	thresholdAttachment.reset();
+	ratioAttachment.reset();
+	attackMsAttachment.reset();
+	releaseMsAttachment.reset();
+	makeUpAttachment.reset();
 }
 
 //==============================================================================
 void _4A2AAudioProcessorEditor::paint(juce::Graphics &g)
 {
-    // fill the whole window white
-    //    g.fillAll (juce::Colours::white);
+	// fill the whole window white
+	//    g.fillAll (juce::Colours::white);
 
-    // set the current drawing colour to black
-    g.setColour(juce::Colours::white);
+	// set the current drawing colour to black
+	g.setColour(juce::Colours::white);
 
-    // set the font size and draw text to the screen
-    g.setFont(25.0f);
+	// set the font size and draw text to the screen
+	g.setFont(25.0f);
 
-    g.drawFittedText("4A-2A", 0, 0, getWidth(), 30,
-                     juce::Justification::centred, 1);
+	g.drawFittedText("4A-2A", 0, 0, getWidth(), 30,
+					 juce::Justification::centred, 1);
 }
 
 void _4A2AAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
-    auto sliderLeft = 120;
-    peakReduction.setBounds(sliderLeft, 40, getWidth() - sliderLeft - 10, 20);
-    threshold.setBounds(sliderLeft, 70, getWidth() - sliderLeft - 10, 20);
-    ratio.setBounds(sliderLeft, 100, getWidth() - sliderLeft - 10, 20);
-    attackMs.setBounds(sliderLeft, 130, getWidth() - sliderLeft - 10, 20);
-    releaseMs.setBounds(sliderLeft, 160, getWidth() - sliderLeft - 10, 20);
-    makeUp.setBounds(sliderLeft, 190, getWidth() - sliderLeft - 10, 20);
+	// This is generally where you'll want to lay out the positions of any
+	// subcomponents in your editor..
+	auto sliderLeft = 120;
+	peakReduction.setBounds(sliderLeft, 40, getWidth() - sliderLeft - 10, 20);
+	threshold.setBounds(sliderLeft, 70, getWidth() - sliderLeft - 10, 20);
+	ratio.setBounds(sliderLeft, 100, getWidth() - sliderLeft - 10, 20);
+	attackMs.setBounds(sliderLeft, 130, getWidth() - sliderLeft - 10, 20);
+	releaseMs.setBounds(sliderLeft, 160, getWidth() - sliderLeft - 10, 20);
+	makeUp.setBounds(sliderLeft, 190, getWidth() - sliderLeft - 10, 20);
 }
 
 std::array<float, 5> _4A2AAudioProcessorEditor::interp(float peakValue)
 {
-    int upper;
-    for (upper = 1; upper < 13; upper++)
-    {
-        if (peakPoints[upper] >= peakValue)
-            break;
-    }
-    auto lower = upper - 1;
-    auto p = (peakValue - peakPoints[lower]) / (peakPoints[upper] - peakPoints[lower]);
-    std::array<float, 5> interpParam;
-    std::transform(std::begin(paramPoints[upper]), std::end(paramPoints[upper]),
-                   std::begin(paramPoints[lower]), interpParam.begin(),
-                   [&p](auto u, auto l)
-                   { return p * u + (1 - p) * l; });
-    return interpParam;
+	int upper;
+	for (upper = 1; upper < 13; upper++)
+	{
+		if (peakPoints[upper] >= peakValue)
+			break;
+	}
+	auto lower = upper - 1;
+	auto p = (peakValue - peakPoints[lower]) / (peakPoints[upper] - peakPoints[lower]);
+	std::array<float, 5> interpParam;
+	std::transform(std::begin(paramPoints[upper]), std::end(paramPoints[upper]),
+				   std::begin(paramPoints[lower]), interpParam.begin(),
+				   [&p](auto u, auto l)
+				   { return p * u + (1 - p) * l; });
+	return interpParam;
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,7 +1,7 @@
 /*
   ==============================================================================
 
-    This file contains the basic framework code for a JUCE plugin editor.
+	This file contains the basic framework code for a JUCE plugin editor.
 
   ==============================================================================
 */
@@ -21,56 +21,56 @@ typedef juce::AudioProcessorValueTreeState::SliderAttachment SliderAttachment;
 class _4A2AAudioProcessorEditor : public juce::AudioProcessorEditor
 {
 public:
-  _4A2AAudioProcessorEditor(_4A2AAudioProcessor &, juce::AudioProcessorValueTreeState &);
-  ~_4A2AAudioProcessorEditor() override;
+	_4A2AAudioProcessorEditor(_4A2AAudioProcessor &, juce::AudioProcessorValueTreeState &);
+	~_4A2AAudioProcessorEditor() override;
 
-  //==============================================================================
-  void paint(juce::Graphics &) override;
-  void resized() override;
+	//==============================================================================
+	void paint(juce::Graphics &) override;
+	void resized() override;
 
 private:
-  // This reference is provided as a quick way for your editor to
-  // access the processor object that created it.
-  _4A2AAudioProcessor &audioProcessor;
-  juce::AudioProcessorValueTreeState &valueTreeState;
+	// This reference is provided as a quick way for your editor to
+	// access the processor object that created it.
+	_4A2AAudioProcessor &audioProcessor;
+	juce::AudioProcessorValueTreeState &valueTreeState;
 
-  std::unique_ptr<SliderAttachment> thresholdAttachment, ratioAttachment,
-      attackMsAttachment, releaseMsAttachment, makeUpAttachment;
-  juce::Slider peakReduction, threshold, ratio, attackMs, releaseMs, makeUp;
-  juce::Label peakReductionLabel, thresholdLabel, ratioLabel, attackMsLabel,
-      releaseMsLabel, makeUpLabel;
+	std::unique_ptr<SliderAttachment> thresholdAttachment, ratioAttachment,
+		attackMsAttachment, releaseMsAttachment, makeUpAttachment;
+	juce::Slider peakReduction, threshold, ratio, attackMs, releaseMs, makeUp;
+	juce::Label peakReductionLabel, thresholdLabel, ratioLabel, attackMsLabel,
+		releaseMsLabel, makeUpLabel;
 
-  float peakPoints[13] = {40, 45, 50, 55, 60, 65, 70,
-                          75, 80, 85, 90, 95, 100};
-  float paramPoints[13][5] = {
-      {-13.075952529907227, 5.435610294342041, 30.516525268554688,
-       470.176513671875, 0.697806715965271},
-      {-14.217720985412598, 4.510651588439941, 17.315229415893555,
-       371.4613342285156, 0.6503340005874634},
-      {-15.320412635803223, 4.193428039550781, 11.579639434814453,
-       324.1263427734375, 0.6253266930580139},
-      {-16.131834030151367, 4.151314735412598, 8.878186225891113,
-       300.0677795410156, 0.6053885817527771},
-      {-16.88168716430664, 4.212928295135498, 7.19603967666626,
-       283.0182189941406, 0.5758121609687805},
-      {-17.586275100708008, 4.265491962432861, 5.948177814483643,
-       266.1832580566406, 0.5347366333007812},
-      {-19.835796356201172, 4.289010047912598, 3.8497214317321777,
-       254.91220092773438, 0.4238012433052063},
-      {-21.904220581054688, 4.237095355987549, 2.8647751808166504,
-       242.99441528320312, 0.33676016330718994},
-      {-24.04867935180664, 4.137104511260986, 2.2430710792541504,
-       234.94378662109375, 0.29632773995399475},
-      {-27.549327850341797, 3.9736087322235107, 1.7689090967178345,
-       248.10983276367188, 0.047231659293174744},
-      {-31.510976791381836, 3.874948501586914, 1.265961766242981,
-       245.48947143554688, 0.03817363455891609},
-      {-33.69828796386719, 3.791938066482544, 1.0862258672714233,
-       244.5567626953125, 0.1468079835176468},
-      {-35.34008026123047, 3.711195945739746, 0.9788835048675537,
-       244.128662109375, 0.3002852201461792}};
+	float peakPoints[13] = {40, 45, 50, 55, 60, 65, 70,
+							75, 80, 85, 90, 95, 100};
+	float paramPoints[13][5] = {
+		{-13.075952529907227, 5.435610294342041, 30.516525268554688,
+		 470.176513671875, 0.697806715965271},
+		{-14.217720985412598, 4.510651588439941, 17.315229415893555,
+		 371.4613342285156, 0.6503340005874634},
+		{-15.320412635803223, 4.193428039550781, 11.579639434814453,
+		 324.1263427734375, 0.6253266930580139},
+		{-16.131834030151367, 4.151314735412598, 8.878186225891113,
+		 300.0677795410156, 0.6053885817527771},
+		{-16.88168716430664, 4.212928295135498, 7.19603967666626,
+		 283.0182189941406, 0.5758121609687805},
+		{-17.586275100708008, 4.265491962432861, 5.948177814483643,
+		 266.1832580566406, 0.5347366333007812},
+		{-19.835796356201172, 4.289010047912598, 3.8497214317321777,
+		 254.91220092773438, 0.4238012433052063},
+		{-21.904220581054688, 4.237095355987549, 2.8647751808166504,
+		 242.99441528320312, 0.33676016330718994},
+		{-24.04867935180664, 4.137104511260986, 2.2430710792541504,
+		 234.94378662109375, 0.29632773995399475},
+		{-27.549327850341797, 3.9736087322235107, 1.7689090967178345,
+		 248.10983276367188, 0.047231659293174744},
+		{-31.510976791381836, 3.874948501586914, 1.265961766242981,
+		 245.48947143554688, 0.03817363455891609},
+		{-33.69828796386719, 3.791938066482544, 1.0862258672714233,
+		 244.5567626953125, 0.1468079835176468},
+		{-35.34008026123047, 3.711195945739746, 0.9788835048675537,
+		 244.128662109375, 0.3002852201461792}};
 
-  std::array<float, 5> interp(float peakValue);
+	std::array<float, 5> interp(float peakValue);
 
-  JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(_4A2AAudioProcessorEditor)
+	JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(_4A2AAudioProcessorEditor)
 };
